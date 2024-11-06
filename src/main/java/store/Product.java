eb48083fe5faaa991c3ec7cddc674b5749b72bcf
SeungminKimdev <ksm7250432@gmail.com>
2024-11-06 21:45:08 +0900

    feat: 상품 정보를 가지는 Product 클래스 생성

---
+++ src/main/java/store/Product.java
@@ -0,0 +1,36 @@
+package store;
+
+public class Product {
+    private String name;
+    private int price;
+    private int quantity;
+    private String promotion;
+
+    public Product(String name, int price, int quantity, String promotion) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.promotion = promotion;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getPromotion() {
+        return promotion;
+    }
+
+    // 변동사항은 재고밖에 없기 때문에 set 사용
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
