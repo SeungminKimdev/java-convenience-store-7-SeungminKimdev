b5c73de0c5d017e27ab6b345c739bb40c816365b
SeungminKimdev <ksm7250432@gmail.com>
2024-11-08 01:26:57 +0900

    feat: 프로모션 불러오는 기능 ProductManager에 추가

--- src/main/java/store/ProductManager.java
+++ src/main/java/store/ProductManager.java
@@ -8,9 +8,11 @@ import java.util.List;
 
 public class ProductManager {
     private List<Product> products = new ArrayList<>();
+    private List<Promotion> promotions = new ArrayList<>();
 
     public ProductManager() {
         loadProducts();
+        loadPromotions();
     }
 
     private void loadProducts() {
@@ -25,6 +27,27 @@ public class ProductManager {
                 if (!line.trim().isEmpty()) {
                     String[] productInfo = line.split(",");
                     products.add(new Product(productInfo[0], productInfo[1], productInfo[2], productInfo[3]));
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("파일을 읽을 수 없습니다.");
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("products 파일에 형식이 맞지 않는 값이 있습니다.");
+        }
+    }
+
+    private void loadPromotions() {
+        try (BufferedReader br = new BufferedReader(new FileReader("src/main/resources/promotions.md"))) {
+            String line;
+
+            if ((line = br.readLine()) != null) {
+                // 첫 줄 건너뛰기
+            }
+
+            while ((line = br.readLine()) != null) {
+                if (!line.trim().isEmpty()) {
+                    String[] promotionInfo = line.split(",");
+                    promotions.add(new Promotion(promotionInfo[0], promotionInfo[1], promotionInfo[2], promotionInfo[3], promotionInfo[4]));
                 }
             }
         } catch (IOException e) {
