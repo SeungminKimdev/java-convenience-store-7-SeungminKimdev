0ba18ae3a6aae77374ef42423514c306d3b3c605
SeungminKimdev <ksm7250432@gmail.com>
2024-11-09 18:51:21 +0900

    feat: 상품 구매 기본 기능 추가

--- src/main/java/store/StoreController.java
+++ src/main/java/store/StoreController.java
@@ -3,13 +3,25 @@ package store;
 public class StoreController {
     private InputView inputView;
     private ProductManager productManager;
+    private Receipt receipt;
 
     public StoreController() {
         inputView = new InputView();
         productManager = new ProductManager();
+        receipt = new Receipt();
     }
 
     private void purchaseProduct() {
-        inputView.readItem();
+        String[] cleanedSplitInput = inputView.readItem().replaceAll("[\\[\\]]", "").split(",");
+        for (String item : cleanedSplitInput) {
+            String[] purchase = item.split("-");
+            int purchaseAmount = Integer.parseInt(purchase[1]);
+            Product purchased = productManager.findProduct(purchase[0]);
+            // 한번에 구매 가능한 경우
+            if (purchased.getQuantity() >= purchaseAmount) {
+                purchased.setQuantity(purchased.getQuantity() - purchaseAmount);
+                receipt.addPurchasedProduct(purchased.getName(), purchased.getQuantity(), purchased.getPrice());
+            }
+        }
     }
 }
