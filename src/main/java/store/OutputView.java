c3523e2b3a67817f2f6bd87c16f4ab39374cd6e1
SeungminKimdev <ksm7250432@gmail.com>
2024-11-09 19:00:35 +0900

    feat: 상품 목록 출력 기능 추가

---
+++ src/main/java/store/OutputView.java
@@ -0,0 +1,30 @@
+package store;
+
+import java.util.List;
+
+public class OutputView {
+    public void showProducts(List<Product> products) {
+        for (Product product : products) {
+            printProducts(product);
+        }
+    }
+
+    private void printProducts(Product product) {
+        String name = product.getName();
+        int price = product.getPrice();
+        int quantity = product.getQuantity();
+        String promotion = product.getPromotion();
+
+        String formattedPrice = String.format("%,d원", price);
+
+        if (quantity == 0) {
+            System.out.printf("- %s %s 재고 없음", name, formattedPrice);
+        } else {
+            System.out.printf("- %s %s %d개", name, formattedPrice, quantity);
+        }
+
+        if (promotion != null) {
+            System.out.printf(" %s\n", promotion);
+        }
+    }
+}
