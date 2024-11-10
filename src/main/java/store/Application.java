63e8e47444f2b5e162aa00c360b0f967d02135a5
SeungminKimdev <ksm7250432@gmail.com>
2024-11-10 17:23:43 +0900

    feat: 메인 기능 실행 추가

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -3,5 +3,6 @@ package store;
 public class Application {
     public static void main(String[] args) {
         StoreController storeController = new StoreController();
+        storeController.openStore();
     }
 }
